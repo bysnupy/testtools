238424659bf9d27d8706140d7ef27c2dc05b1665
Daein <bysnupy@hotmail.com>
2020-10-30 12:57:44 +0900

    add new app to record time inifinitely

---
+++ running-datetime-recorder/datetime_recorder.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type StatusRecorder struct {
+	http.ResponseWriter
+	Status int
+}
+
+func (r *StatusRecorder) WriteHeader(status int) {
+	r.Status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		recorder := &StatusRecorder{
+			ResponseWriter: w,
+			Status:         200,
+		}
+		h.ServeHTTP(recorder, r)
+		log.Printf("Handling request for %s from %s, status: %d", r.URL.Path, r.RemoteAddr, recorder.Status)
+	})
+}
+
+func dateTimeRecorder() {
+	for {
+		currentTime := time.Now().String()
+		fmt.Println(currentTime)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	httpRequestHandler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		response := "Server response time: " + time.Now().String()
+		fmt.Fprintln(resp, response)
+	})
+	handlerWithLogging := WithLogging(httpRequestHandler)
+	http.Handle("/", handlerWithLogging)
+	defaultPort := os.Getenv("PORT")
+	if len(defaultPort) == 0 {
+		defaultPort = "8080"
+	}
+	err := http.ListenAndServe(":"+defaultPort, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	dateTimeRecorder()
+}
