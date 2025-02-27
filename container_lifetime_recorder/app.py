0dd08e97c16d9456e03410d9434561536ff29eb0
Daein Park <bysnupy@hotmail.com>
2025-02-27 13:04:17 +0900

    Update app.py

--- container_lifetime_recorder/app.py
+++ container_lifetime_recorder/app.py
@@ -4,23 +4,17 @@
 * Author: Daein Park
 """
 
-from flask import Flask
-from flask import request
-from threading import Thread
 import sys,os,time,signal
-app = Flask(__name__)
 
 
-listening_port = 8080
 container_name = "default"
 
-if os.environ.get('FLASK_RUN_PORT'):
-    listening_port = int(os.environ.get('FLASK_RUN_PORT'))
 if os.environ.get('CONTAINER_NAME'):
     container_name = str(os.environ.get('CONTAINER_NAME'))
-    
+
 def sig_print(signum, frame):
     print(time.strftime('%H:%M:%S'), ": RECEIVED a signal : ", signal.Signals(signum).name)
+    sys.exit(0)
 
 catchable_sigs = set(signal.Signals)
 for sig in catchable_sigs:
@@ -37,10 +31,5 @@ if os.environ.get('EXIT_SUCCESS'):
     print(time.strftime('%H:%M:%S'), ": STOPPED with 0 exit code, Container name: ", container_name);
     sys.exit(0)
 
-@app.route('/', defaults={'req_path': ''})
-@app.route('/<path:req_path>')
-def request_main(req_path):
-    return "OK"
-
-if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=listening_port, debug=True)
+time.sleep(3600)
+sys.exit(0)
